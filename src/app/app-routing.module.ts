70023db1d7c9910e0d91d800fa212d186fbb81f5
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2023-03-08 18:25:59 +0530

    new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, 
+  { path: 'customer', loadChildren: () => import('./cutomer/cutomer.module').then(m => m.CutomerModule) }, 
+  { path: 'expert', loadChildren: () => import('./expert/expert.module').then(m => m.ExpertModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
